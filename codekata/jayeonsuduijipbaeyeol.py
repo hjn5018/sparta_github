5cf010ee6338c5d3dd1f1423d53ff305f7c49e9b
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-03-05 13:35:52 +0900

    이거 등록해야하는데,..........

--- codekata/jayeonsuduijipbaeyeol.py
+++ codekata/jayeonsuduijipbaeyeol.py
@@ -3,9 +3,83 @@
 
 # n은 10,000,000,000이하인 자연수입니다.
 
-def solution(n):
-    list_ = []
-    for i in str(n):
-        
+# def solution(n):
+#     list_ = []
+#     for i in n:
+#         print()
 
-print(solution(542))
+# print(solution(542))
+
+# ==================================================
+# n = 12345
+# list_ = []
+# for i in n:
+# #     print(i)
+# # # TypeError: 'int' object is not iterable
+# ==================================================
+    
+# n = 12345
+# list_ = []
+# print(list(n))
+# # TypeError: 'int' object is not iterable
+# ======================================================
+# n = 12345
+# list_ = []
+# n = str(n)
+# # print(type(n))
+# # # <class 'str'>
+# for i in n:
+#     list_.append(i)
+# # print(list_)
+# # # ['1', '2', '3', '4', '5']
+# ==========================================================
+# n = 12345
+# list_ = []
+# n = str(n)
+
+# for i in n:
+#     list_.append(i)
+# print(list_)
+# # ['1', '2', '3', '4', '5']
+# list_[::-1]
+# print(list_)
+# # ['1', '2', '3', '4', '5']
+# # ???????????????????????????????????????????????????/
+# ====================================================================
+# n = 12345
+# list_ = []
+# n = str(n)
+
+# for i in n:
+#     list_.append(i)
+# #     print(list_[int(i)-1])
+# # # 1
+# # # 2
+# # # 3
+# # # 4
+# # # 5
+# ============================================================
+# n = 12345
+# list_ = []
+# n = str(n)
+
+# for i in n:
+#     list_.append(int(i))
+# # print(list_)
+# # # [1, 2, 3, 4, 5]
+# ======================================================
+n = 12345
+list_ = []
+n = str(n)
+
+for i in n:
+    list_.append(int(i))
+print(list_[::-1])
+
+def solution(n);
+    list_ = []
+    n = str(n)
+
+    for i in n:
+        list_.append(int(i))
+    return list_
