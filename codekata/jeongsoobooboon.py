b04c607240c5b6eccec2f871417bc027c185d8fc
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-03-05 18:41:28 +0900

    정수부분

---
+++ codekata/jeongsoobooboon.py
@@ -0,0 +1,14 @@
+# 실수 flo가 매개 변수로 주어질 때,
+# flo의 정수 부분을 return하도록 solution 함수를 완성해주세요.
+
+# 0 ≤ flo ≤ 100
+
+flo = 1.42
+# flo = 69.32
+
+# result = type(flo)
+# print(result)
+# # <class 'float'>
+
+def solution(flo):
+    return int(flo)
