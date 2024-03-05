4e8fc27067eba4b3d33e5328c46650f49f364cd0
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-03-05 17:22:25 +0900

    옷가게..

--- codekata/otgagehalin.py
+++ codekata/otgagehalin.py
@@ -40,7 +40,7 @@
 # # UnboundLocalError: local variable 'reduce' referenced before assignment
 # ============================================
 # list_ = [100000, 300000, 500000]
-dict_ = {100000:0.05, 300000:0.1, 500000:0.2}
+# dict_ = {100000:0.05, 300000:0.1, 500000:0.2}
 # print(dict_[0])
 # # KeyError: 0
 
@@ -53,12 +53,53 @@ dict_ = {100000:0.05, 300000:0.1, 500000:0.2}
 # # 0.1
 # # 0.2
 
-price = 150000
+# price = 150000
 
-if price > 500000:
-    reduce = 0.2
-elif price > 300000:
-    reduce = 0.1
-elif price > 100000:
-    reduce = 0.05
-price = price * (1 - reduce)
+# if price > 500000:
+#     reduce = 0.2
+# elif price > 300000:
+#     reduce = 0.1
+# elif price > 100000:
+#     reduce = 0.05
+# price = price * (1 - reduce)
+
+# print(price)
+# ㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠㅠ
+
+def solution(price):
+    if price >= 500000:
+        reduce = 0.2
+    elif price >= 300000:
+        reduce = 0.1
+    elif price >= 100000:
+        reduce = 0.05
+    else:
+        reduce = 0
+    price = price * (1 - reduce)
+    price = int(price)
+    return price
+# # =======한별님 코드===========
+# def solution(price):
+#     if price >= 500000:
+#         answer = price * .80
+#     elif price >= 300000:
+#         answer = price* .90
+#     elif price >= 100000:
+#         answer = price* .95
+#     else:
+#         answer = price
+#     return int(answer)
+# # =============준성님 코드=========
+# import math
+# def solution(price):
+#     answer = 0
+#     if 100000 <= price < 300000:
+#         answer = math.floor(price * 0.95)
+#     elif 300000 <= price < 500000:
+#         answer = math.floor(price * 0.9)
+#     elif price >= 500000:
+#         answer = math.floor(price * 0.8)
+#     else:
+#         answer = price #실수 조심
+#     return answer
+
