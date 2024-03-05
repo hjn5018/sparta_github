6f3e6bfc3d115f38bcea5b43f23ad7505c6f5ab5
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-03-05 19:29:50 +0900

    소문자로 바꾸기

---
+++ codekata/somoonjarobaggoogi.py
@@ -0,0 +1,25 @@
+# 알파벳으로 이루어진 문자열 myString이 주어집니다.
+# 모든 알파벳을 소문자로 변환하여 return 하는 solution 함수를 완성해 주세요.
+
+# 1 ≤ myString의 길이 ≤ 100,000
+# myString은 알파벳으로 이루어진 문자열입니다.
+
+myString = "aBcDeFg"
+# result = "abcdefg"
+# lower_case = "abcdefghijklmnopqrstuvwxyz"
+# upper_case = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+
+# result = lower_case in myString
+# # False
+
+# for i in myString:
+#     if i in upper_case:
+#         for j in lower_case:
+#             if j == 
+
+
+# print(result)
+# 구현하기는 좀 어렵네
+# ==============================================
+def solution(myString):
+    return myString.lower()
