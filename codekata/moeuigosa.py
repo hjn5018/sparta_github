ffb934083edb8c3888c96b1a16ec793379ecb45b
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-03-05 14:15:07 +0900

    예린튜터님 세션 사전 공부

---
+++ codekata/moeuigosa.py
@@ -0,0 +1,28 @@
+def solution(answers):
+    n = len(answers)
+    
+    count_1 = 0
+    1_supoja = []
+    while n != len(1_supoja):
+        for i in range(5):
+            1_supoja.append(i+1)
+    
+    for i in range(n):
+        if answer[i] == l_supoja[i]
+        count += 1
+    
+    count_2 = 0
+    2_supoja = []
+    while n !=len(2_supoja):
+        for i in range(8):
+            if i % 2 == 0:
+                2_supoja.append(2)
+            elif i == 1:
+                2_supoja.append(1)
+            elif i == 3:
+                2_supoja.append(3)
+            elif i == 5:
+                2_supoja.append(4)
+            else:
+                2_supoja.append(5)
+                
