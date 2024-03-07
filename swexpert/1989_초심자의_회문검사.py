acf6b807a3223e1a0dfa5eb70ff8181435e28e83
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-03-07 13:14:26 +0900

    완성???

--- swexpert/1989_초심자의_회문검사.py
+++ swexpert/1989_초심자의_회문검사.py
@@ -77,14 +77,14 @@
 # eye
 # #3  eye
 # ===============이제 한번에 출력하기만 하면 됨.=====================================
-case_num = int(input())
-for i, j in enumerate(range(3), 1): # 인풋 다 받아버리기 // enumerate로 index 새겨주기
-    case = input() # 근데 index를 어떻게 꽂아주지..?
+# case_num = int(input())
+# for i, j in enumerate(range(3), 1): # 인풋 다 받아버리기 // enumerate로 index 새겨주기
+#     case = input() # 근데 index를 어떻게 꽂아주지..?
 
-    if case == case[::-1]:
-        print(f"#{i}, 1")
-    else:
-        print(f"#{i} 0")
+#     if case == case[::-1]:
+#         print(f"#{i}, 1")
+#     else:
+#         print(f"#{i} 0")
 # 3
 # level
 # #1, 1
@@ -92,4 +92,42 @@ for i, j in enumerate(range(3), 1): # 인풋 다 받아버리기 // enumerate로
 # #2 0
 # eye
 # #3, 1
-# ======================================================
+# ============str_에 담는 건 아닌듯.. 계속 초기화 될 뿐임..==========================================
+# case_num = int(input())
+# str_ = ""
+
+# for i, j in enumerate(range(3), 1): # 인풋 다 받아버리기 // enumerate로 index 새겨주기
+#     case = input() # 근데 index를 어떻게 꽂아주지..?
+
+#     if case == case[::-1]:
+#         str_ + f"#{i}, 1"
+#         # print(f"#{i}, 1")
+#     else:
+#         str_ + f"#{i}, 0"
+#         # print(f"#{i} 0")
+# ==========================================================
+case_num = int(input())
+list_ = []
+
+for i, j in enumerate(range(3), 1): # 인풋 다 받아버리기 // enumerate로 index 새겨주기
+    case = input() # 근데 index를 어떻게 꽂아주지..?
+
+    if case == case[::-1]:
+        list_.append(f"#{i} 1")
+        # list_.append(f"#{i}, 1")
+        # print(f"#{i}, 1")
+    else:
+        list_.append(f"#{i} 0")
+        # list_.append(f"#{i}, 0")
+        # print(f"#{i} 0")
+
+for k in list_:
+    print(k)
+
+#1, 1
+#2, 0
+#3, 1
+# 콤마 떼자!!!
+#1 1
+#2 0
+#3 1
