fda2862710c2fd3d2a8d50051222717206a1c63f
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-03-07 17:54:21 +0900

    뽑아서 더하는 걸... 어케함..?

---
+++ swexpert/2817_부분_수열의_합.py
@@ -0,0 +1,27 @@
+T = int(input())
+
+N, K = map(int, input().split())
+
+case = list(map(int, input().split()))
+
+case[0]
+case[1]
+case[2]
+case[3]
+
+# for i in case:
+#     i
+
+# for j in range(N):
+#     case[j]
+
+# combination(case) -> [case[0] ... (case[0]+case[1]), ... (case[0]+...case[3])]
+
+
+# for i in range(N):
+    # i개 만큼 원소를 뽑는다.
+    # result = pick(case, i)
+    # 뽑은 걸 더한다.
+    # sigma(result)
+
+# 
