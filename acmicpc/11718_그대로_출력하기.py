75aef32be58fcb0496fba4cb7bf6271f8a2403d5
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-03-11 19:15:44 +0900

    런타임...

---
+++ acmicpc/11718_그대로_출력하기.py
@@ -0,0 +1,34 @@
+# for _ in range(100):
+#     s = input()
+
+#     print(s)
+# 런타임 에러 (EOFError)
+# ================================
+# while s is True:
+#     s = input()
+
+#     print(s)
+# NameError: name 's' is not defined
+# ====================================
+# print(bool(""))
+# # False
+# print(bool("12"))
+# # True
+# ===================================
+# for _ in range(100):
+#     s = input()
+    
+#     print(s)
+
+#     if s is False:
+#         break
+# 런타임 에러 (EOFError)
+# ====================================
+# while 1:
+#     s = input()
+
+#     print(s)
+#     if s is False:
+#         break
+    # 런타임 에러 (EOFError)
+# ======================================
