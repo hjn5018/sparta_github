36d30228b2b3c6f92477ac0947d6ff153ab06100
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-03-11 11:21:48 +0900

    백준 연습

---
+++ acmicpc/10172.py
@@ -0,0 +1,34 @@
+# print("|\_/|")
+# print("|q p|   /}")
+# print("( 0 )"""\")
+# print("|"^"`    |")
+# print("||_/=\\__|")
+# SyntaxError: unexpected character after line continuation character
+# ========================
+# print("|\_/|")
+# print("|q p|   /}")
+# print("( 0 )\"\"\"\")
+# print("|"^"`    |")
+# print("||_/=\\__|")
+# SyntaxError: EOL while scanning string literal
+# ===========================
+# print("|\_/|")
+# print("|q p|   /}")
+# print("( 0 )\"\"\"")
+# print("|\"^\"`    |")
+# print("||_/=\\__|")
+# 틀렸습니다
+# ==========================
+# print("|\_/|")
+# print("|q p|   /}")
+# print("( 0 )\"\"\"\\")
+# print("|\"^\"`    |")
+# print("||_/=\\__|")
+# 틀렸습니다
+# ==================================
+print("|\_/|")
+print("|q p|   /}")
+print("( 0 )\"\"\"\\")
+print("|\"^\"`    |")
+print("||_/=\\\__|")
+
---
+++ acmicpc/18108.py
@@ -0,0 +1,5 @@
+test_case = input()
+
+result = int(test_case) - 543
+
+print(result)
