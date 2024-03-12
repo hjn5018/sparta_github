b62bf88f576a7a84c4509dd8cbc6f7b93a599b2a
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-03-12 09:43:45 +0900

    큐 - 시간초과

---
+++ acmicpc/2164_카드2.py
@@ -0,0 +1,72 @@
+# N = int(input())
+
+# cards = []
+# for i in range(1, N+1):
+#     cards.append(i)
+# # print(cards)
+# # [1, 2, 3, 4, 5, 6]
+    
+# for _ in range(N-1):
+#     cards.pop(0)
+#     cards.append(cards.pop(0))
+# # cards.pop(0)
+# # print(cards)
+# # [2, 3, 4, 5, 6]
+    
+# # print(cards.pop(0))
+# # 1
+
+# # cards.pop(0)
+# # cards.append(cards.pop(0))
+# # print(cards)
+# # [3, 4, 5, 6, 2]
+
+# print(cards)
+# # [4]
+
+# # 시간 초과
+# ========================================
+# import sys
+# # N = int(input())
+# N = int(sys.stdin.readline())
+
+# cards = []
+# for i in range(1, N+1):
+#     cards.append(i)
+# # print(cards)
+# # [1, 2, 3, 4, 5, 6]
+    
+# for _ in range(N-1):
+#     cards.pop(0)
+#     cards.append(cards.pop(0))
+# # cards.pop(0)
+# # print(cards)
+# # [2, 3, 4, 5, 6]
+    
+# # print(cards.pop(0))
+# # 1
+
+# # cards.pop(0)
+# # cards.append(cards.pop(0))
+# # print(cards)
+# # [3, 4, 5, 6, 2]
+
+# # print(cards)
+# # [4]
+# sys.stdout.write(f"{cards}")
+# # 시간 초과
+# ==========================================
+import sys
+
+N = int(sys.stdin.readline())
+
+cards = []
+for i in range(1, N+1):
+    cards.append(i)
+    
+for _ in range(N-1):
+    cards.pop(0)
+    cards.append(cards.pop(0))
+
+sys.stdout.write(f"{cards}")
+# 시간 초과 // pypy3도 시간초과
