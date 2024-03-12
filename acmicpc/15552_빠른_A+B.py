24bf1e7339103d0d6f828774d574bd0178253856
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-03-12 18:58:45 +0900

    sys입출력

---
+++ acmicpc/15552_빠른_A+B.py
@@ -0,0 +1,29 @@
+# import sys
+
+# T = int(sys.stdin.readline())
+
+# for _ in range(T):
+#     A, B = map(int, sys.stdin.readline().split())
+
+#     sys.stdout.write(f"{A + B}")
+# 2465051002000
+# ==============================================
+import sys
+
+T = int(sys.stdin.readline())
+
+for _ in range(T):
+    A, B = map(int, sys.stdin.readline().split())
+
+    sys.stdout.write(f"{A + B}\n")
+# 5
+# 1 1
+# 2
+# 12 34
+# 46
+# 5 500
+# 505
+# 40 60
+# 100
+# 1000 1000
+# 2000
