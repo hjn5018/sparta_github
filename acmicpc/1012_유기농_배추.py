274edb166bb8dc177c20321dbe1701de2b527327
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-03-13 13:19:05 +0900

    배추 썩겠다

---
+++ acmicpc/1012_유기농_배추.py
@@ -0,0 +1,21 @@
+import sys
+
+T = int(sys.stdin.readline())
+M, N, K = map(int, sys.stdin.readline())
+
+# 상하좌우 = [(0, 1), (0, -1), (-1, 0), (1, 0)]
+상하좌우 = [[0, 1], [0, -1], [-1, 0], [1, 0]]
+for _ in range(T):
+    farm = []
+    for _ in range(K):
+        pos = list(map(int, sys.stdin.readline().split()))
+        farm.append(pos)
+    memoi = []
+    for i in range(K):
+        if farm[i] not in memoi:
+            memoi.append(farm[i])
+            for j in range(len(상하좌우)):
+                farm[i] += 상하좌우[j]
+                if farm[i] in farm:
+                    farm.append(farm[i])
+# memoi를 쓰려고해도 O(j^i)는 좀 아니지 않나..?
