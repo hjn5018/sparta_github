55432e02c26f15272cb249eb75e9dc4a852efd87
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-03-14 15:42:25 +0900

    그렇군 완전히..

--- swexpert/5174_subtree.py
+++ swexpert/5174_subtree.py
@@ -1,18 +1,18 @@
-import sys
+# import sys
 
-T = int(sys.stdin.readline())
+# T = int(sys.stdin.readline())
 
-for _ in range(1, T+1):
-    E, N = map(int, sys.stdin.readline().split())
-    pair_list = list(map(int, sys.stdin.readline().split()))
+# for _ in range(1, T+1):
+#     E, N = map(int, sys.stdin.readline().split())
+#     pair_list = list(map(int, sys.stdin.readline().split()))
 
-    keys = []
-    values = []
-    for i in range(len(pair_list)):
-        if i % 2 == 0:
-            keys.append(pair_list[i])
-        else:
-            values.append(pair_list[i])
+#     keys = []
+#     values = []
+#     for i in range(len(pair_list)):
+#         if i % 2 == 0:
+#             keys.append(pair_list[i])
+#         else:
+#             values.append(pair_list[i])
     # print(keys)
     # print(values)
     # [2, 2, 1, 5, 6]
@@ -23,5 +23,4 @@ for _ in range(1, T+1):
     #     pair_dict[keys[i]] = values[i]
     # print(pair_dict)
     # {2: 5, 1: 6, 5: 3, 6: 4}
-    
-    
+# ============================================================
