fca80a1f09363e3da122fe8884ac3c8051d89874
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-03-18 21:05:58 +0900

    원소가 리스트인 리스트 다루기

---
+++ codekata_programmers/ysj.py
@@ -0,0 +1,22 @@
+# def solution(arr, intervals):
+#     answer = []
+#     for i in intervals:
+#         i[1] += 1
+#         for j in range(i):
+#             answer.append(j)
+#     return answer
+
+		
+arr = [1, 2, 3, 4, 5]
+intervals = [[1, 3], [0, 4]]
+# result = [2, 3, 4, 1, 2, 3, 4, 5]
+
+
+list_ = []
+for i in intervals:
+    i[1] += 1
+    for j in range(i):
+        list_.append(j)
+
+print(list_)
+
