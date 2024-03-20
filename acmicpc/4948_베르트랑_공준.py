ed43012a58397c7ae20fa7c0e20dc0ae1f0daa2d
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-03-20 14:38:21 +0900

    소수검증, 1포함???

---
+++ acmicpc/4948_베르트랑_공준.py
@@ -0,0 +1,33 @@
+# count = 0
+# while 1:
+#     N = int(input())
+#     if N == 0:
+#         break
+
+#     num_list = []
+#     for i in range(N, 2*N + 1):
+#         num_list.append(i)
+#         # num_list = [N, N+1, ... 2N]
+
+#     for i in num_list:
+#         for j in range(1, i+1):
+#             if i % j == 0:
+#                 continue
+#             else:
+#                 count += 1
+#     print(count)
+
+
+
+# print(list(range(1,2)))
+    
+
+
+소수 검증
+a가 소수이려면
+a 이하의 모든 수로 나눴을 때 나머지가 0이 아니다.
+for i in range(1, a+1):
+    # if a % i !=0:
+    #     continue
+    # else: # a % i == 0: continue
+검증 마치면 count += 1
