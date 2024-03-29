56a6173e8a49971ffd44f836addf6256d1fe72c1
hjn5018 <75594057+hjn5018@users.noreply.github.com>
2024-03-29 09:34:28 +0900

    my_string[::-1]해도 됨;;

--- codekata_programmers/문자열_여러_번_뒤집기.py
+++ codekata_programmers/문자열_여러_번_뒤집기.py
@@ -1,4 +1,6 @@
-def solution(my_string, queries):
-    for s, e in queries:
-        my_string = my_string[:s] + ''.join(list(my_string[s:e+1])[::-1]) + my_string[e+1:]
-    return my_string
+# def solution(my_string, queries):
+#     for s, e in queries:
+#         my_string = my_string[:s] + ''.join(list(my_string[s:e+1])[::-1]) + my_string[e+1:]
+#     return my_string
+
+my_string[::-1]해도 됨;;
